e9300db5ee4d8db8a662068f0aaffa2400339939
RaiymbekKokteubay <rkokteubai@gmail.com>
2022-10-08 15:03:14 +0600

    minor changes

--- bot.py
+++ bot.py
@@ -1,34 +1,38 @@
 import telebot
+import os
 from telebot import types
 
-api_key = '5435243295:AAEDsQRyUsBg8Cmyii0NfR5j4RJJXUJ_wv8'
+api_key = os.getenv('TELEGRAM_API')
+print(api_key)
 bot = telebot.TeleBot(api_key)
 
-# @bot.message_handler(commands=["start"])
-# def start(message):
-#   start_message = f"<b>Hello</b>, <i>{message.from_user.first_name}</i>"
-#   bot.send_message(message.chat.id, start_message, parse_mode='html')
+b2t = {
+  "hotlines": "☎️Горячие Линии☎️",
+  "government": "Государственная служба",
+  "assist": "Гумманитарная помощь" 
+}
 
 @bot.message_handler(commands=['start'])
 def start(message):
   markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-  item1 = types.KeyboardButton('1')
-  item2 = types.KeyboardButton('2')
-  item3 = types.KeyboardButton('3')
-  item4 = types.KeyboardButton('4')
-  item5 = types.KeyboardButton('5')
-  
-
-  markup.add(item1, item2, item3, item4, item5)
+  item1 = types.KeyboardButton(b2t["hotlines"])
+  item2 = types.KeyboardButton(b2t["government"])
+  item3 = types.KeyboardButton(b2t["assist"])
+  markup.add(item1, item2, item3)
   start_message = f"<b>Hello</b>, <i>{message.from_user.first_name}</i>"
   bot.send_message(message.chat.id, start_message, reply_markup = markup, parse_mode="html")
 
 @bot.message_handler(content_types=['text'])
 def bot_message(message):
   if message.chat.type == 'private':
-    if message.text == '1':
-      bot.send_message(message.chat.id, '1')
+
+    if message.text == b2t["hotlines"]:
+      with open("replies/hotlines.txt") as f:
+        reply = f.read()
+      bot.send_message(message.chat.id, reply, parse_mode="html")
+
     elif message.text == '2':
+
       markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
       item1 = types.KeyboardButton('1')
       item2 = types.KeyboardButton('2')
