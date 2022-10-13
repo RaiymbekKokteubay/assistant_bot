891d40192fe0cbabe73a0ba343fcfd1755265f85
RaiymbekKokteubai <rkokteubai@gmail.com>
2022-10-13 17:33:39 +0600

    added buttons

--- bot.py
+++ bot.py
@@ -1,112 +1,76 @@
 import telebot
 import os
+import json
 from telebot import types
 
+responses = json.load(open("responses.json", encoding="utf-8"))
+buttons = json.load(open("buttons.json", encoding="utf-8"))
 api_key = os.getenv('TELEGRAM_API')
-print(api_key)
 bot = telebot.TeleBot(api_key)
-
-b2t = {
-  "hotlines": "☎️Оперативные службы в Казахстане☎️",
-  "government": "Государственная служба",
-  "assist": "Гумманитарная помощь" 
-}
 
 @bot.message_handler(commands=['start'])
 def start(message):
-  markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-  item1 = types.KeyboardButton(b2t["hotlines"])
-  item2 = types.KeyboardButton(b2t["government"])
-  item3 = types.KeyboardButton(b2t["assist"])
-  markup.add(item1, item2, item3)
-  start_message = f"<b>Hello</b>, <i>{message.from_user.first_name}</i>"
-  bot.send_message(message.chat.id, start_message, reply_markup = markup, parse_mode="html")
+  markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+  item1 = types.KeyboardButton("ua")
+  item2 = types.KeyboardButton("ru")
+  markup.add(item1, item2)
+  bot.send_message(
+    message.chat.id,
+    "Выберите язык / Choose language",
+    reply_markup=markup,
+    parse_mode="html"
+  )
 
 @bot.message_handler(content_types=['text'])
 def bot_message(message):
   if message.chat.type == 'private':
 
-    if message.text == b2t["hotlines"]:
-      with open("replies/hotlines.txt", encoding="utf-8") as f:
-        reply = f.read()
+    if message.text in ["ru", "ua", buttons["back"]["ru"]]:
+      markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+      item1 = types.KeyboardButton(buttons["hotlines"]["ru"])
+      item2 = types.KeyboardButton(buttons["government"]["ru"])
+      item3 = types.KeyboardButton(buttons["assist"]["ru"])
+      markup.add(item1, item2, item3)
+      start_message = f"Теперь выберите категорию"
+      bot.send_message(
+        message.chat.id,
+        start_message,
+        reply_markup=markup,
+        parse_mode="html"
+      )
+
+    elif message.text == buttons["hotlines"]["ru"]:
+      reply = responses["hotlines"]["ru"]
       bot.send_message(message.chat.id, reply, parse_mode="html")
 
-    elif message.text == b2t["government"]:
+    elif message.text == buttons["government"]["ru"]:
+      markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+      item1 = types.KeyboardButton(buttons["bank"]["ru"])
+      item2 = types.KeyboardButton(buttons["psc"]["ru"])
+      item3 = types.KeyboardButton(buttons["embassy"]["ru"])
+      item4 = types.KeyboardButton(buttons["back"]["ru"])
+      markup.add(item1, item2, item3, item4)
+      bot.send_message(
+        message.chat.id,
+        "Выберите подходящую категорию",
+        reply_markup=markup,
+        parse_mode="html"
+      )
 
-      markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-      item1 = types.KeyboardButton('Банк')
-      item2 = types.KeyboardButton('ЦОН')
-      item3 = types.KeyboardButton('Посольство')
-      back = types.KeyboardButton('назад')
-      markup.add(item1, item2, item3, back)
-
-      bot.send_message(message.chat.id, start_message, reply_markup = markup, parse_mode="html")
-
-      if message.text ==('Банк'):
-        with open("replies/Bank.txt", encoding="utf-8") as f:
-          reply = f.read()
-        bot.send_message(message.chat.id, reply, parse_mode="html")
-
-      elif message.text == ('ЦОН'):
-        with open("replies/PSC.txt", encoding="utf-8") as f:
-         reply = f.read()
-        bot.send_message(message.chat.id, reply, parse_mode="html")
-
-      elif message.text == ('Посольство'):
-        with open("replies/embassy.txt", encoding="utf-8") as f:
-         reply = f.read()
-        bot.send_message(message.chat.id, reply, parse_mode="html")
-    
-      elif message.text == b2t["assist"]:
-        with open("replies/funds.txt", encoding="utf-8") as f:
-         reply = f.read()
+    elif message.text == buttons["bank"]["ru"]:
+      reply = responses["bank"]["ru"]
       bot.send_message(message.chat.id, reply, parse_mode="html")
-    
-    elif message.text == '3':
-      markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-      item1 = types.KeyboardButton('1')
-      item2 = types.KeyboardButton('2')
-      item3 = types.KeyboardButton('3')
-      item4 = types.KeyboardButton('4')
-      back = types.KeyboardButton('назад')
-      markup.add(item1, item2, item3, item4, back)
-      start_message = f"<b>Hello</b>, <i>{message.from_user.first_name}</i>"
-  
-      bot.send_message(message.chat.id, start_message, reply_markup = markup, parse_mode="html")
 
-    elif message.text == '4':
-      markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-      item1 = types.KeyboardButton('1')
-      item2 = types.KeyboardButton('2')
-      item3 = types.KeyboardButton('3')
-      item4 = types.KeyboardButton('4')
-      back = types.KeyboardButton('назад')
-      markup.add(item1, item2, item3, item4, back)
-      start_message = "4"
-    
-      bot.send_message(message.chat.id, start_message, reply_markup = markup, parse_mode="html")
+    elif message.text == buttons["psc"]["ru"]:
+      reply = responses["psc"]["ru"]
+      bot.send_message(message.chat.id, reply, parse_mode="html")
 
-    elif message.text == '5':
-      markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-      item1 = types.KeyboardButton('1')
-      item2 = types.KeyboardButton('2')
-      item3 = types.KeyboardButton('3')
-      item4 = types.KeyboardButton('4')
-      back = types.KeyboardButton('назад')
-      markup.add(item1, item2, item3, item4, back)
-      start_message = f"<b>Hello</b>, <i>{message.from_user.first_name}</i>"
-      bot.send_message(message.chat.id, start_message, reply_markup = markup, parse_mode="html")
+    elif message.text == buttons["embassy"]["ru"]:
+      reply = responses["embassy"]["ru"]
+      bot.send_message(message.chat.id, reply, parse_mode="html")
 
-    elif message.text == 'назад':
-      markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-      item1 = types.KeyboardButton('1')
-      item2 = types.KeyboardButton('2')
-      item3 = types.KeyboardButton('3')
-      item4 = types.KeyboardButton('4')
-      item5 = types.KeyboardButton('5')
-      markup.add(item1, item2, item3, item4, item5)
-      start_message = "назад"
-      bot.send_message(message.chat.id, start_message, reply_markup = markup, parse_mode="html")
-  
-  
+    elif message.text == buttons["funds"]["ru"]:
+      reply = responses["funds"]["ru"]
+      bot.send_message(message.chat.id, reply, parse_mode="html")
+
 bot.polling(non_stop=True,)
