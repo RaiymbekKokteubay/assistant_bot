e6afae0294eac632b20997737f08433934d85c0d
RaiymbekKokteubai <rkokteubai@gmail.com>
2022-11-14 22:19:02 +0600

    Small changes

--- bot.py
+++ bot.py
@@ -95,68 +95,6 @@ def bot_message(message):
 			bot.send_message(message.chat.id, reply, parse_mode="html")
 		elif message.text == buttons["assist"][lang]:
 			reply = responses["assist"][lang]
-			bot.send_message(message.chat.id, reply, parse_mode="html"
-				  markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-  item1 = types.KeyboardButton("ua")
-  item2 = types.KeyboardButton("ru")
-  markup.add(item1, item2)
-  bot.send_message(
-    message.chat.id,
-    "Выберите язык",
-    reply_markup=markup,
-    parse_mode="html"
-  )
-
-@bot.message_handler(content_types=['text'])
-def bot_message(message):
-  if message.chat.type == 'private':
-
-    if message.text in ["ua", "ru", buttons["back"]["ua"]]:
-      markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-      item1 = types.KeyboardButton(buttons["hotlines"]["ua"])
-      item2 = types.KeyboardButton(buttons["government"]["ua"])
-      item3 = types.KeyboardButton(buttons["assist"]["ua"])
-      markup.add(item1, item2, item3)
-      start_message = f"Теперь выберите категорию"
-      bot.send_message(
-        message.chat.id,
-        start_message,
-        reply_markup=markup,
-        parse_mode="html"
-      )
-
-    elif message.text == buttons["hotlines"]["ua"]:
-      reply = responses["hotlines"]["ua"]
-      bot.send_message(message.chat.id, reply, parse_mode="html")
-
-    elif message.text == buttons["government"]["ua"]:
-      markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-      item1 = types.KeyboardButton(buttons["bank"]["ua"])
-      item2 = types.KeyboardButton(buttons["psc"]["ua"])
-      item3 = types.KeyboardButton(buttons["embassy"]["ua"])
-      item4 = types.KeyboardButton(buttons["back"]["ua"])
-      markup.add(item1, item2, item3, item4)
-      bot.send_message(
-        message.chat.id,
-        "Выберите подходящую категорию",
-        reply_markup=markup,
-        parse_mode="html"
-      )
-
-    elif message.text == buttons["bank"]["ua"]:
-      reply = responses["bank"]["ua"]
-      bot.send_message(message.chat.id, reply, parse_mode="html")
-
-    elif message.text == buttons["psc"]["ua"]:
-      reply = responses["psc"]["ua"]
-      bot.send_message(message.chat.id, reply, parse_mode="html")
-
-    elif message.text == buttons["embassy"]["ua"]:
-      reply = responses["embassy"]["ua"]
-      bot.send_message(message.chat.id, reply, parse_mode="html")
-
-    elif message.text == buttons["assist"]["ua"]:
-      reply = responses["assist"]["ua"]
-      bot.send_message(message.chat.id, reply, parse_mode="html")
+			bot.send_message(message.chat.id, reply, parse_mode="html")
 
 bot.polling(non_stop=True,)
